1bf60abe1724f54e0e2c0266cce60d88be0ebf1d
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-03-12 16:50:14 -0300

    Criando modelo Tarefa

---
+++ API/src/main/java/api/tarefa/model/Tarefa.java
@@ -0,0 +1,50 @@
+package api.tarefa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tarefa")
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", columnDefinition = "serial")
+    private Long id;
+    @Column(name = "titulo")
+    private String titulo;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "status")
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
