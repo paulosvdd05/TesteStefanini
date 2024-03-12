2373ac1a83cdbe82d26b66830402930500ff127e
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-03-12 17:00:15 -0300

    Criando TarefaDTO

---
+++ API/src/main/java/api/tarefa/repository/TarefaDTO.java
@@ -0,0 +1,41 @@
+package api.tarefa.repository;
+
+public class TarefaDTO {
+    private Long id;
+    private String titulo;
+    private String descricao;
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
