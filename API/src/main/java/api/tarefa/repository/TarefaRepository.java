6ae17025f558e1b19e68d7811cc5b79c1e908a54
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-03-12 16:56:11 -0300

    Criando TarefaService

--- API/src/main/java/api/tarefa/repository/TarefaRepository.java
+++ API/src/main/java/api/tarefa/repository/TarefaRepository.java
@@ -1,9 +1,10 @@
 package api.tarefa.repository;
 
+import api.tarefa.model.Tarefa;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import api.tarefa.model.Tarefa;
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
---
+++ API/src/main/java/api/tarefa/repository/TarefaService.java
@@ -0,0 +1,10 @@
+package api.tarefa.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TarefaService {
+    @Autowired
+    private TarefaRepository repository;
+}
